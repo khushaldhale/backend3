ab1c11cba54a4a8b79667fa4c970f3b55889b1ce
khushaldhale <khushaldhale999@gmail.com>
2024-05-20 12:07:52 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+require("dotenv").config()
+app.use(express.json());
+
+
+
+
+app.get("/", (req, res) => {
+	return res.status(200)
+		.json({
+			success: true,
+			message: "server is up and running"
+		})
+})
+
+
+app.get("/greet", (req, res) => {
+	return res.status(200)
+		.json({
+			success: true,
+			message: "we  are greeting you"
+		})
+})
+
+
+const dbConnect = require("./config/database");
+dbConnect()
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, () => {
+	console.log("server is listening at PORT: ", PORT)
+})
